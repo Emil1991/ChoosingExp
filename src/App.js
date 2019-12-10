0a2681d752c96d95be7142e6a63f9b611767aa4f
Emil <emilgulayev@gmail.com>
2019-12-10 21:17:15 +0200

    tailwind integrated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import Button from "./components/Button";
 
 function App() {
   return (
-    <div className="App">
-      HELLO
+    <div className="flex flex-col w-3/4 mx-auto my-12 items-center">
+      <h1>Super cool page</h1>
+      <Button onClick={() => console.log("I was clicked")}>
+        I am a button
+      </Button>
     </div>
   );
 }
