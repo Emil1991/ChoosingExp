60b5da15f9189008f4a41e454ca53e9eb77715f5
Emil <emilgulayev@gmail.com>
2019-12-10 22:00:10 +0200

    extra tailwind

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Button({ children, ...buttonProps }) {
   return (
     <button
-      className="px-2 py-1 rounded-lg bg-green-400 text-green-800 text-xl font-light uppercase shadow-md hover:shadow-lg"
+      className="px-3 py-1 rounded-lg bg-blue-500 text-blue-800 text-xl font-light uppercase shadow-md hover:shadow-lg"
       {...buttonProps}
     >
       {children}
