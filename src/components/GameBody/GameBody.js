5fea283346baed3abaa7e829f37ac685f92272e1
Emil <emilgulayev@gmail.com>
2019-12-16 22:22:56 +0200

    continue

--- src/components/GameBody/GameBody.js
+++ src/components/GameBody/GameBody.js
@@ -26,7 +26,9 @@ class GameBody extends Component {
     render() {
         return(
                 <div id="GameBody" className="br-1 r"> 
-                Body
+                <div className="br-1"><b>Instructions:</b> Click to select or unselect a choice. You may select up to 1 choice.</div>
+                <div className="br-1">BODY</div>
+                <div className="br-1"><b>Instructions:</b> Double check your ballot. Then click the "Vote!" button to cast your vote.</div>
                 {/* {this.props.children} */}
                 </div>
         )
--- src/components/RewardSection/RewardSection.js
+++ src/components/RewardSection/RewardSection.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
+import "./RewardSection.css"
 
 
-
-class RewardSection extends Component {
+class  RewardSection extends Component {
     constructor(props) {
         super(props);
         this.state = {
